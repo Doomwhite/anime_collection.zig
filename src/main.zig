9e32a580ec28786c2718d98fa2dd3535d04a7655
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-09-24 19:19:22 -0300

    Makes the variables global

--- src/main.zig
+++ src/main.zig
@@ -4,90 +4,20 @@ const raylib = @import("raylib");
 const Image = raylib.Image;
 const Allocator = std.mem.Allocator;
 
-// const settings = @import("settings");
-// const ColorTheme = settings.ColorTheme;
+const settings = @import("settings.zig");
+const ColorTheme = settings.ColorTheme;
+const menu_width = settings.menu_width;
+const MenuItem = settings.MenuItem;
 
-pub const ColorTheme = struct {
-    title: []const u8, // The text to display for the menu item
-    dm_text_color: ?raylib.Color,
-    lm_text_color: ?raylib.Color,
-    dm_background_color: ?raylib.Color,
-    lm_background_color: ?raylib.Color,
-    dm_hover_color: ?raylib.Color,
-    lm_hover_color: ?raylib.Color,
-    dm_selected_color: ?raylib.Color,
-    lm_selected_color: ?raylib.Color,
-
-    pub fn text(self: ColorTheme) raylib.Color {
-        if (is_dark_mode) {
-            return self.dm_text_color orelse raylib.WHITE;
-        } else {
-            return self.lm_text_color orelse raylib.WHITE;
-        }
-    }
-
-    pub fn background(self: ColorTheme) raylib.Color {
-        if (is_dark_mode) {
-            return self.dm_background_color orelse raylib.WHITE;
-        } else {
-            return self.lm_background_color orelse raylib.WHITE;
-        }
-    }
-
-    pub fn hover(self: ColorTheme) raylib.Color {
-        if (is_dark_mode) {
-            return self.dm_hover_color orelse raylib.WHITE;
-        } else {
-            return self.lm_hover_color orelse raylib.WHITE;
-        }
-    }
-
-    pub fn selected(self: ColorTheme) raylib.Color {
-        if (is_dark_mode) {
-            return self.dm_selected_color orelse raylib.WHITE;
-        } else {
-            return self.lm_selected_color orelse raylib.WHITE;
-        }
-    }
-};
-
-const colors = [_]ColorTheme{
-    ColorTheme{
-        .title = "Menu items",
-        .dm_text_color = raylib.RED, // Dark mode text color for this item
-        .lm_text_color = raylib.BLACK, // Light mode text color for this item
-        .dm_background_color = raylib.BLACK, // Dark mode background color (null for default)
-        .lm_background_color = raylib.WHITE, // Light mode background color (null for default)
-        .dm_hover_color = raylib.GREEN, // Dark mode hover color
-        .lm_hover_color = raylib.LIME, // Light mode hover color
-        .dm_selected_color = raylib.RED, // Dark mode selected color
-        .lm_selected_color = raylib.RED, // Light mode selected color
-    },
-};
-
-// Define a struct to represent menu items
-const MenuItem = struct {
-    title: [*:0]const u8, // The text to display for the menu item
-    selected: bool, // Indicates whether the item is currently selected
-};
-
-// Create an array of menu items
-var menu_items = [3]MenuItem{
-    MenuItem{ .title = "Browse Anime", .selected = false },
-    MenuItem{ .title = "Browse Manga", .selected = false },
-    MenuItem{ .title = "Settings", .selected = false },
-};
-
-const menu_width = 200; // Width of the sidebar menu
-pub var is_dark_mode: bool = true;
+var selected_index: usize = 0;
 
 pub fn main() !void {
+    settings.init_globals();
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() == .leak) @panic("found memory leaks");
     const allocator: Allocator = gpa.allocator();
 
     var db = try sqlite.Db.init(.{
-        // .mode = sqlite.Db.Mode{ .File = "/database.db" },
         .mode = sqlite.Db.Mode{ .File = "/database.db" },
         .open_flags = .{
             .write = true,
@@ -96,47 +26,43 @@ pub fn main() !void {
         .threading_mode = .MultiThread,
     });
     defer db.deinit();
-    // defer std.log.info("gpa.deinit(): {any}", .{gpa.deinit()});
-    // defer std.log.info("gpa.deinit(): {any}", .{gpa.detectLeaks()});
-    // defer if (gpa.deinit() == .leak) {
-    //     std.debug.panic("leaks detected", .{});
-    // };
 
     try executeSQLiteScripts(&db, allocator);
 
-    // defer raylib.CloseWindow();
-    //
-    // const iconImage: Image = raylib.LoadImage("sailor.png");
-    // defer raylib.UnloadImage(iconImage);
-    // std.log.info("iconImage: {any}", .{iconImage});
-    //
-    // raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
-    // raylib.InitWindow(800, 800, "Anime collection.zig");
-    // raylib.SetWindowIcon(iconImage);
-    // raylib.SetTargetFPS(60);
-    //
-    // var selected_index: usize = 0;
-    //
-    // while (!raylib.WindowShouldClose()) {
-    //     raylib.BeginDrawing();
-    //     defer raylib.EndDrawing();
-    //
-    //     raylib.ClearBackground(colors[0].background());
-    //     raylib.DrawFPS(10, 10);
-    //     renderSidebarMenu(&selected_index);
-    //
-    //     // raylib.DrawText("hello world!", 100, 100, 20, raylib.YELLOW);
-    // }
+    defer raylib.CloseWindow();
+
+    const iconImage: Image = raylib.LoadImage("sailor.png");
+    defer raylib.UnloadImage(iconImage);
+    std.log.info("iconImage: {any}", .{iconImage});
+
+    raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
+    raylib.InitWindow(1600, 900, "Anime collection.zig");
+    raylib.SetWindowIcon(iconImage);
+    raylib.SetTargetFPS(144);
+
+    while (!raylib.WindowShouldClose()) {
+        raylib.BeginDrawing();
+        defer raylib.EndDrawing();
+
+        raylib.ClearBackground(settings.colors[0].background());
+        raylib.DrawFPS(10, 10);
+        renderSidebarMenu();
+
+        // raylib.DrawText("hello world!", 100, 100, 20, raylib.YELLOW);
+    }
 }
 
-fn renderSidebarMenu(selected_index: *usize) void {
+fn renderSidebarMenu() void {
     // Calculate the position for the menu items
     var menu_item_y: i32 = 100;
 
-    for (menu_items, 0..) |item, menu_index| {
+    for (settings.menu_items, 0..) |item, menu_index| {
         // Determine the text color based on whether the item is selected or hovered
         var text_color: raylib.Color = undefined;
 
+        if (raylib.IsMouseButtonPressed(raylib.MouseButton.MOUSE_BUTTON_RIGHT)) {
+            settings.is_dark_mode = !settings.is_dark_mode;
+        }
         // Check if the mouse cursor is over the menu item
         if (raylib.CheckCollisionPointRec(raylib.GetMousePosition(), raylib.Rectangle{
             .x = 0,
@@ -144,11 +70,20 @@ fn renderSidebarMenu(selected_index: *usize) void {
             .width = menu_width,
             .height = 20,
         })) {
-            text_color = colors[0].hover(); // Change the text color when hovered
-        } else if (selected_index.* == menu_index) {
-            text_color = colors[0].selected(); // Change the text color when selected
+            text_color = settings.colors[0].hover(); // Change the text color when hovered
+
+            // Check if the left mouse button was clicked
+            if (raylib.IsMouseButtonPressed(raylib.MouseButton.MOUSE_BUTTON_LEFT)) {
+                // Handle the click event here for the current menu item
+                // You can use the `menu_index` variable to identify which item was clicked
+                // For example:
+                std.log.info("Clicked on menu item {d}", .{menu_index});
+                selected_index = menu_index;
+            }
+        } else if (selected_index == menu_index) {
+            text_color = settings.colors[0].selected(); // Change the text color when selected
         } else {
-            text_color = colors[0].text(); // Default text color
+            text_color = settings.colors[0].text(); // Default text color
         }
 
         // Draw the menu item text
@@ -177,43 +112,6 @@ fn executeSQLiteScripts(db: *sqlite.Db, gpa_allocator: Allocator) !void {
     // // Execute the SQL command to create the table
     // try db.exec(createTableScript, .{}, .{});
 
-    // const insertScript =
-    //     \\INSERT INTO Anime (title
-    //     \\,genre
-    //     \\)
-    //     \\VALUES(
-    //     \\,$title{[]const u8}
-    //     \\,$genre{[]const u8}
-    //     \\)
-    // ;
-
-    // Execute the SQL command to create the table
-    // try db.exec(insertScript, .{}, .{ @as([]const u8, "Kindaichi"), @as([]const u8, "Mystery") });
-    //
-    // const selectScript =
-    //     \\ SELECT
-    //     \\ id
-    //     \\ ,title
-    //     \\ FROM
-    //     \\ Anime
-    // ;
-
-    // var stmt = try db.prepare(selectScript);
-    // defer stmt.deinit();
-    //
-    // const row = try stmt.one(
-    //     struct {
-    //         id: usize,
-    //         title: [128:0]u8,
-    //     },
-    //     .{},
-    //     .{},
-    // );
-    //
-    // if (row) |row_value| {
-    //     std.log.info("row1: {s}", .{row_value.title});
-    // }
-
     const query =
         \\SELECT 
         \\id 
@@ -226,19 +124,7 @@ fn executeSQLiteScripts(db: *sqlite.Db, gpa_allocator: Allocator) !void {
     var stmt = try db.prepare(query);
     defer stmt.deinit();
 
-    // const row2 = try db.oneAlloc(
-    //     struct {
-    //       id: usize,
-    //       title: []const u8,
-    //       genre: []const u8,
-    //     },
-    //     allocator,
-    //     query,
-    //     .{},
-    //     .{},
-    // );
-
-    const row2 = try stmt.oneAlloc(
+    const row = try stmt.oneAlloc(
         struct {
             id: usize,
             title: []const u8,
@@ -249,10 +135,10 @@ fn executeSQLiteScripts(db: *sqlite.Db, gpa_allocator: Allocator) !void {
         .{},
     );
 
-    if (row2) |row2_value| {
-        std.log.info("row2_value.id: {any}", .{row2_value.id});
-        std.log.info("row2_value.title: {s}", .{row2_value.title});
-        std.log.info("row2_value.genre: {s}", .{row2_value.genre});
+    if (row) |row_value| {
+        std.log.info("row2_value.id: {any}", .{row_value.id});
+        std.log.info("row2_value.title: {s}", .{row_value.title});
+        std.log.info("row2_value.genre: {s}", .{row_value.genre});
     }
 
     var iter_stmt = try db.prepare(query);
--- src/settings.zig
+++ src/settings.zig
@@ -1,48 +1,83 @@
-// const raylib = @import("raylib");
-// const Image = raylib.Image;
-//
-// extern const is_dark_mode: bool;
+const raylib = @import("raylib");
+const Image = raylib.Image;
 
-// pub const ColorTheme = struct {
-//     title: []const u8, // The text to display for the menu item
-//     dm_text_color: ?raylib.Color,
-//     lm_text_color: ?raylib.Color,
-//     dm_background_color: ?raylib.Color,
-//     lm_background_color: ?raylib.Color,
-//     dm_hover_color: ?raylib.Color,
-//     lm_hover_color: ?raylib.Color,
-//     dm_selected_color: ?raylib.Color,
-//     lm_selected_color: ?raylib.Color,
-//
-//     pub fn text(self: ColorTheme) raylib.Color {
-//         if (is_dark_mode) {
-//             return self.dm_text_color orelse raylib.WHITE;
-//         } else {
-//             return self.lm_text_color orelse raylib.WHITE;
-//         }
-//     }
-//
-//     pub fn background(self: ColorTheme) raylib.Color {
-//         if (is_dark_mode) {
-//             return self.dm_background_color orelse raylib.WHITE;
-//         } else {
-//             return self.lm_background_color orelse raylib.WHITE;
-//         }
-//     }
-//
-//     pub fn hover(self: ColorTheme) raylib.Color {
-//         if (is_dark_mode) {
-//             return self.dm_hover_color orelse raylib.WHITE;
-//         } else {
-//             return self.lm_hover_color orelse raylib.WHITE;
-//         }
-//     }
-//
-//     pub fn selected(self: ColorTheme) raylib.Color {
-//         if (is_dark_mode) {
-//             return self.dm_selected_color orelse raylib.WHITE;
-//         } else {
-//             return self.lm_selected_color orelse raylib.WHITE;
-//         }
-//     }
-// };
+pub var is_dark_mode: bool = false;
+
+pub const ColorTheme = struct {
+    title: []const u8, // The text to display for the menu item
+    dm_text_color: ?raylib.Color,
+    lm_text_color: ?raylib.Color,
+    dm_background_color: ?raylib.Color,
+    lm_background_color: ?raylib.Color,
+    dm_hover_color: ?raylib.Color,
+    lm_hover_color: ?raylib.Color,
+    dm_selected_color: ?raylib.Color,
+    lm_selected_color: ?raylib.Color,
+
+    pub fn text(self: ColorTheme) raylib.Color {
+        if (is_dark_mode) {
+            return self.dm_text_color orelse raylib.WHITE;
+        } else {
+            return self.lm_text_color orelse raylib.WHITE;
+        }
+    }
+
+    pub fn background(self: ColorTheme) raylib.Color {
+        if (is_dark_mode) {
+            return self.dm_background_color orelse raylib.WHITE;
+        } else {
+            return self.lm_background_color orelse raylib.WHITE;
+        }
+    }
+
+    pub fn hover(self: ColorTheme) raylib.Color {
+        if (is_dark_mode) {
+            return self.dm_hover_color orelse raylib.WHITE;
+        } else {
+            return self.lm_hover_color orelse raylib.WHITE;
+        }
+    }
+
+    pub fn selected(self: ColorTheme) raylib.Color {
+        if (is_dark_mode) {
+            return self.dm_selected_color orelse raylib.WHITE;
+        } else {
+            return self.lm_selected_color orelse raylib.WHITE;
+        }
+    }
+};
+
+// Define a struct to represent menu items
+pub const MenuItem = struct {
+    title: [*:0]const u8, // The text to display for the menu item
+    selected: bool, // Indicates whether the item is currently selected
+};
+
+pub var colors: [1]ColorTheme = undefined;
+
+// Create an array of menu items
+pub var menu_items: [3]MenuItem = undefined;
+
+// pub var is_dark_mode: bool = true;
+pub const menu_width = 200; // Width of the sidebar menu
+
+pub fn init_globals() void {
+    menu_items = [3]MenuItem{
+        MenuItem{ .title = "Browse Anime", .selected = false },
+        MenuItem{ .title = "Browse Manga", .selected = false },
+        MenuItem{ .title = "Settings", .selected = false },
+    };
+    colors = [_]ColorTheme{
+        ColorTheme{
+            .title = "Menu items",
+            .dm_text_color = raylib.RED, // Dark mode text color for this item
+            .lm_text_color = raylib.BLACK, // Light mode text color for this item
+            .dm_background_color = raylib.BLACK, // Dark mode background color (null for default)
+            .lm_background_color = raylib.WHITE, // Light mode background color (null for default)
+            .dm_hover_color = raylib.GREEN, // Dark mode hover color
+            .lm_hover_color = raylib.LIME, // Light mode hover color
+            .dm_selected_color = raylib.PURPLE, // Dark mode selected color
+            .lm_selected_color = raylib.LIME, // Light mode selected color
+        },
+    };
+}
