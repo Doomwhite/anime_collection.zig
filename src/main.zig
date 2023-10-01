c501e0f6d8fe310a632b99877b79c9bd77e317cc
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-10-01 02:04:39 -0300

    Adds menu item logic

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,7 @@ const settings = @import("settings.zig");
 const ColorTheme = settings.ColorTheme;
 const menu_width = settings.menu_width;
 const MenuItem = settings.MenuItem;
+const MenuItemType = settings.MenuItemType;
 
 const db = @import("db.zig");
 
@@ -46,23 +47,15 @@ pub fn main() !void {
     raylib.SetTargetFPS(144);
 
     while (!raylib.WindowShouldClose()) {
-        // if (haha) |hihi| {
-        //     settings.current_menu = hihi;
-        //     haha = null;
-        // }
         raylib.BeginDrawing();
         defer raylib.EndDrawing();
 
         raylib.ClearBackground(settings.colors[settings.selected_theme_index].background());
         raylib.DrawFPS(10, 10);
         if (raylib.IsMouseButtonPressed(raylib.MouseButton.MOUSE_BUTTON_RIGHT)) {
-            settings.changeTheme();
-            std.log.info("settings.current_menu[selected_index]: {s}", .{settings.current_menu[selected_index].title});
-            if (settings.current_menu.len == 3) {
-                settings.current_menu = settings.menu_items[2].items.?;
-            } else {
-                settings.current_menu = &settings.menu_items;
-            }
+            // var current_menu = settings.current_menu;
+            // settings.current_menu = settings.previous_menu;
+            // settings.previous_menu = current_menu;
         }
         // renderSidebarMenu();
         var menu_item_y: i32 = 100;
@@ -71,23 +64,31 @@ pub fn main() !void {
             // Determine the text color based on whether the item is selected or hovered
             var text_color: raylib.Color = undefined;
 
+            // std.log.info("menu_item_y: {any}", .{menu_item_y});
             // Check if the mouse cursor is over the menu item
-            if (raylib.CheckCollisionPointRec(raylib.GetMousePosition(), raylib.Rectangle{
-                .x = 0,
-                .y = @floatFromInt(menu_item_y),
-                .width = menu_width,
-                .height = 20,
-            })) {
+            if (cursorInColission(menu_item_y)) {
                 text_color = settings.colors[settings.selected_theme_index].hover(); // Change the text color when hovered
-
-                // Check if the left mouse button was clicked
                 if (raylib.IsMouseButtonPressed(raylib.MouseButton.MOUSE_BUTTON_LEFT)) {
-                    // Handle the click event here for the current menu item
-                    // You can use the `menu_index` variable to identify which item was clicked
-                    // For example:
-                    // std.log.info("Clicked on menu item {d}", .{menu_index});
-                    // selected_index = menu_index;
-                    // std.log.info("item: {any}", .{item});
+                    switch (item.type) {
+                        .Nothing => {},
+                        .Settings => {
+                            if (item.items) |children_items| {
+                                settings.previous_menu = settings.current_menu;
+                                settings.current_menu = children_items;
+                                break;
+                            }
+                        },
+                        .ChangeTheme => {
+                            settings.changeTheme();
+                        },
+                        .Return => {
+                            var current_menu = settings.current_menu;
+                            settings.current_menu = settings.previous_menu;
+                            settings.previous_menu = current_menu;
+                            break;
+                        },
+                    }
+                    selected_index = menu_index;
                     // if (item.items) |item_children| {
                     //     // settings.current_menu = item_children.items;
                     //     haha = item_children;
@@ -103,53 +104,62 @@ pub fn main() !void {
             raylib.DrawText(item.title, 10, menu_item_y, 20, text_color);
 
             // Increase the Y position for the next menu item
-            menu_item_y += 30;
+            menu_item_y += settings.menu_item_height;
         }
 
         // raylib.DrawText("hello world!", 100, 100, 20, raylib.YELLOW);
     }
 }
 
-fn renderSidebarMenu() void {
-    // Calculate the position for the menu items
-    var menu_item_y: i32 = 100;
-
-    for (settings.current_menu, 0..) |item, menu_index| {
-        // Determine the text color based on whether the item is selected or hovered
-        var text_color: raylib.Color = undefined;
-
-        // Check if the mouse cursor is over the menu item
-        if (raylib.CheckCollisionPointRec(raylib.GetMousePosition(), raylib.Rectangle{
-            .x = 0,
-            .y = @floatFromInt(menu_item_y),
-            .width = menu_width,
-            .height = 20,
-        })) {
-            text_color = settings.colors[settings.selected_theme_index].hover(); // Change the text color when hovered
-
-            // Check if the left mouse button was clicked
-            if (raylib.IsMouseButtonPressed(raylib.MouseButton.MOUSE_BUTTON_LEFT)) {
-                // Handle the click event here for the current menu item
-                // You can use the `menu_index` variable to identify which item was clicked
-                // For example:
-                std.log.info("Clicked on menu item {d}", .{menu_index});
-                selected_index = menu_index;
-                std.log.info("item: {any}", .{item});
-                if (item.items) |item_children| {
-                    // settings.current_menu = item_children.items;
-                    haha = item_children;
-                }
-            }
-        } else if (selected_index == menu_index) {
-            text_color = settings.colors[settings.selected_theme_index].selected(); // Change the text color when selected
-        } else {
-            text_color = settings.colors[settings.selected_theme_index].text(); // Default text color
-        }
-
-        // Draw the menu item text
-        raylib.DrawText(item.title, 10, menu_item_y, 20, text_color);
-
-        // Increase the Y position for the next menu item
-        menu_item_y += 30;
-    }
+fn cursorInColission(menu_item_y: i32) bool {
+    return raylib.CheckCollisionPointRec(raylib.GetMousePosition(), raylib.Rectangle{
+        .x = 0,
+        .y = @floatFromInt(menu_item_y),
+        .width = menu_width,
+        .height = 20,
+    });
 }
+
+// fn renderSidebarMenu() void {
+//     // Calculate the position for the menu items
+//     var menu_item_y: i32 = 100;
+//
+//     for (settings.current_menu, 0..) |item, menu_index| {
+//         // Determine the text color based on whether the item is selected or hovered
+//         var text_color: raylib.Color = undefined;
+//
+//         // Check if the mouse cursor is over the menu item
+//         if (raylib.CheckCollisionPointRec(raylib.GetMousePosition(), raylib.Rectangle{
+//             .x = 0,
+//             .y = @floatFromInt(menu_item_y),
+//             .width = menu_width,
+//             .height = 20,
+//         })) {
+//             text_color = settings.colors[settings.selected_theme_index].hover(); // Change the text color when hovered
+//
+//             // Check if the left mouse button was clicked
+//             if (raylib.IsMouseButtonPressed(raylib.MouseButton.MOUSE_BUTTON_LEFT)) {
+//                 // Handle the click event here for the current menu item
+//                 // You can use the `menu_index` variable to identify which item was clicked
+//                 // For example:
+//                 std.log.info("Clicked on menu item {d}", .{menu_index});
+//                 selected_index = menu_index;
+//                 std.log.info("item: {any}", .{item});
+//                 if (item.items) |item_children| {
+//                     // settings.current_menu = item_children.items;
+//                     haha = item_children;
+//                 }
+//             }
+//         } else if (selected_index == menu_index) {
+//             text_color = settings.colors[settings.selected_theme_index].selected(); // Change the text color when selected
+//         } else {
+//             text_color = settings.colors[settings.selected_theme_index].text(); // Default text color
+//         }
+//
+//         // Draw the menu item text
+//         raylib.DrawText(item.title, 10, menu_item_y, 20, text_color);
+//
+//         // Increase the Y position for the next menu item
+//         menu_item_y += 30;
+//     }
+// }
--- src/settings.zig
+++ src/settings.zig
@@ -44,39 +44,50 @@ pub const ColorTheme = struct {
 
 // Define a struct to represent menu items
 pub const MenuItem = struct {
+    type: MenuItemType, // Indicates whether the item is currently selected
     parent: ?*MenuItem, // Indicates whether the item is currently selected
     title: [*:0]const u8, // The text to display for the menu item
     selected: bool, // Indicates whether the item is currently selected
-    items: ?[2]*MenuItem, // Indicates whether the item is currently selected
+    items: ?[]const MenuItem, // Indicates whether the item is currently selected
 };
+
+pub const menu_item_height = 30;
 
 pub var colors: [3]ColorTheme = undefined;
 
 // Create an array of menu items
-pub var menu_items: [3]MenuItem = undefined;
-pub var teste_items = [2]MenuItem{
-    MenuItem{ .parent = null, .title = "Change theme", .selected = false, .items = null },
-    MenuItem{ .parent = null, .title = "Change theme haha", .selected = false, .items = null },
-};
 
 pub var current_menu: []const MenuItem = undefined;
+pub var previous_menu: []const MenuItem = undefined;
+pub var menu_items: []const MenuItem = undefined;
+
+pub const MenuItemType = enum {
+    Nothing,
+    Settings,
+    ChangeTheme,
+    Return,
+};
 
 // pub var is_dark_mode: bool = true;
 pub const menu_width = 200; // Width of the sidebar menu
 
 pub fn init_globals() !void {
-    var settings_menu_item = MenuItem{ .parent = null, .title = "Settings", .selected = false, .items = null };
-    teste_items[0].parent = &settings_menu_item;
-    teste_items[1].parent = &settings_menu_item;
-    settings_menu_item.items = [2]*MenuItem{ &teste_items[0], &teste_items[1] };
-    menu_items = [3]MenuItem{
-        MenuItem{ .parent = null, .title = "Browse Anime", .selected = false, .items = null },
-        MenuItem{ .parent = null, .title = "Browse Manga", .selected = false, .items = null },
-        settings_menu_item,
+    menu_items = &[_]MenuItem{
+        MenuItem{ .type = .Nothing, .parent = null, .title = "Browse Anime", .selected = false, .items = null },
+        MenuItem{ .type = .Nothing, .parent = null, .title = "Browse Manga", .selected = false, .items = null },
+        MenuItem{
+            .type = .Settings,
+            .parent = null,
+            .title = "Settings",
+            .selected = false,
+            .items = &[_]MenuItem{
+                MenuItem{ .type = .ChangeTheme, .parent = null, .title = "Change theme", .selected = false, .items = null },
+                MenuItem{ .type = .Return, .parent = null, .title = "Return", .selected = false, .items = null },
+            },
+        },
     };
-    // current_menu = ArrayList(MenuItem).init(allocator);
-    // try current_menu.appendSlice(&menu_items);
-    current_menu = &menu_items;
+
+    current_menu = menu_items;
 
     colors = [_]ColorTheme{
         ColorTheme{
